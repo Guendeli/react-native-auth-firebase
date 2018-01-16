683cda2cd04f33ddf7ffc64389658e884444854b
GUENDELI Omar <guendeliomar@gmail.com>
2018-01-16 03:03:28 +0000

    Client Side Wrapup

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,19 +1,38 @@
 import React, {Component} from 'react';
-import {View} from 'react-native';
-import {Button, Card, CardItem} from './common';
+import {Button, Card, CardItem, InputField} from './common';
 
 class LoginForm extends Component 
 {
+    state = {email: '', password: ''}
+
+
     render()
     {
         return(
             <Card>
                 <CardItem>
+                    <InputField 
+                        placeholder="user@mail.com"
+                        label="Email"
+                        value = {this.state.email}
+                        onChangeText = {
+                            text => this.setState({email: text})
+                        }
+                    >
 
+                    </InputField>
                 </CardItem>
 
                 <CardItem>
-                    
+                    <InputField
+                        secureTextEntry={true}
+                        placeholder="password"
+                        label="Password"
+                        value = {this.state.password}
+                        onChangeText = {
+                            text => this.setState({password: text})
+                        }
+                    />
                 </CardItem>
 
                 <CardItem>
