3f2178456435ce289c8cd09690c0f4f77fe86126
GUENDELI Omar <guendeliomar@gmail.com>
2018-01-16 04:17:37 +0000

    Firebase integration, user registration, authentification error handling

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,10 +1,62 @@
 import React, {Component} from 'react';
-import {Button, Card, CardItem, InputField} from './common';
+import {Text} from 'react-native';
+import firebase from 'firebase';
+import {Button, Card, CardItem, InputField, Spinner} from './common';
 
 class LoginForm extends Component 
 {
-    state = {email: '', password: ''}
+    state = {email: '', password: '', error: '', loading: false}
 
+    onButtonPress()
+    {
+        // deconstructing state params
+        const {email, password} = this.state;
+        this.setState({error: '', loading: true});
+
+        // Attempt to sign in
+        firebase.auth().signInWithEmailAndPassword(email, password)
+        .then(this.onLoginSucess.bind(this))
+        .catch(() => {
+            //in case of error, let's just create an account, for the sake of learning
+            firebase.auth().createUserWithEmailAndPassword(email, password)
+            .then(this.onLoginSucess.bind(this))
+            .catch(this.onLoginFail.bind(this));
+        });
+    }
+
+    onLoginSucess(){
+        this.setState({
+            email: '',
+            password: '',
+            error: '',
+            loading: false
+        });
+    }
+
+    onLoginFail(){
+        // well, sum tin wong, let's show an error message
+        this.setState({
+            error: "Authentification Failed.",
+            loading: false
+        });
+    }
+
+    renderButton()
+    {
+        if(this.state.loading){
+            return(
+                <Spinner size='small'/>
+            );
+        } else {
+            return(
+                <Button
+                onPress = {this.onButtonPress.bind(this)}
+                >
+                    Log In
+                </Button>
+            );
+        }
+    }
 
     render()
     {
@@ -16,7 +68,7 @@ class LoginForm extends Component
                         label="Email"
                         value = {this.state.email}
                         onChangeText = {
-                            text => this.setState({email: text})
+                            email => this.setState({email})
                         }
                     >
 
@@ -35,14 +87,24 @@ class LoginForm extends Component
                     />
                 </CardItem>
 
+                <Text style = {styles.errorTextStyle} >
+                    {this.state.error}
+                </Text>
+
                 <CardItem>
-                    <Button>
-                        Log In
-                    </Button>
+                   {this.renderButton()}
                 </CardItem>
 
             </Card>
         );
+    }
+}
+
+const styles = {
+    errorTextStyle: {
+        fontSize: 20,
+        alignSelf: 'center',
+        color: 'red'
     }
 }
 
--- src/components/common/index.js
+++ src/components/common/index.js
@@ -3,3 +3,4 @@ export * from './Card';
 export * from './Header';
 export * from './CardItem';
 export * from './InputField';
+export * from './Spinner';
